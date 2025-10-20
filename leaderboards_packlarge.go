43124ddd849eb2e9beae5f8bddcba047b43be406
Michael Carlson <corfe83@gmail.com>
2025-10-20 12:21:07 -0700

    Refactor code cleaner to reduce duplication

--- leaderboards_packlarge.go
+++ leaderboards_packlarge.go
@@ -1,18 +1,18 @@
-//go:build !windows
+//go:build windows && !386
 
 package steamworks
 
-// non-windows platforms (linux, apple, freebsd) use VALVE_CALLBACK_PACK_SMALL as defined in steamclientpublic.h
-// All of these structs follow strict padding, without using cgo, to match VALVE_CALLBACK_PACK_SMALL
-// This means every struct member, if smaller than 4 bytes, is padded to 4 bytes
+// 64-bit Windows uses VALVE_CALLBACK_PACK_LARGE as defined in steamclientpublic.h (8-byte alignment)
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+)
 
 type leaderboardFindResult_t struct {
 	// m_hSteamLeaderboard: 8 bytes
-	// m_bLeaderboardFound: 1 byte (4 bytes padded)
-	// total: 12 bytes
-	data [12]byte
+	// m_bLeaderboardFound: 1 byte (8 bytes padded)
+	// total: 16 bytes
+	data [16]byte
 }
 
 func (me leaderboardFindResult_t) Read() leaderboardFindResult {
@@ -25,9 +25,9 @@ func (me leaderboardFindResult_t) Read() leaderboardFindResult {
 type leaderboardScoresDownloaded_t struct {
 	// m_hSteamLeaderboard:        8 bytes
 	// m_hSteamLeaderboardEntries: 8 bytes
-	// m_cEntryCount:              4 bytes
-	// total: 20 bytes
-	data [20]byte
+	// m_cEntryCount:              4 bytes (8 bytes padded)
+	// total: 24 bytes
+	data [24]byte
 }
 
 func (me leaderboardScoresDownloaded_t) Read() leaderboardScoresDownloaded {
@@ -39,35 +39,35 @@ func (me leaderboardScoresDownloaded_t) Read() leaderboardScoresDownloaded {
 }
 
 type leaderboardScoreUploaded_t struct {
-	// m_bSuccess:            1 byte (4 bytes padded)
-	// m_hSteamLeaderboard:   8 bytes
-	// m_nScore:              4 bytes
-	// m_bScoreChanged:       1 byte (4 bytes padded)
-	// m_nGlobalRankNew:      4 bytes
-	// m_nGlobalRankPrevious: 4 bytes
-	// total: 28 bytes
-	data [28]byte
+	// m_bSuccess:            1 byte  (4 bytes padded)
+	// m_hSteamLeaderboard:   8 bytes (12 bytes padded)
+	// m_nScore:              4 bytes (8 bytes padded)
+	// m_bScoreChanged:       1 byte  (4 bytes padded)
+	// m_nGlobalRankNew:      4 bytes (4 bytes padded)
+	// m_nGlobalRankPrevious: 4 bytes (4 bytes padded)
+	// total: 48 bytes
+	data [48]byte
 }
 
 func (me leaderboardScoreUploaded_t) Read() leaderboardScoreUploaded {
 	var result leaderboardScoreUploaded
 	result.success = me.data[0] != 0
 	result.steamLeaderboard = SteamLeaderboard_t(binary.NativeEndian.Uint64(me.data[4:12]))
-	result.score = int32(binary.NativeEndian.Uint32(me.data[12:16]))
-	result.scoreChanged = me.data[16] != 0
-	result.globalRankNew = int32(binary.NativeEndian.Uint32(me.data[20:24]))
-	result.globalRankPrevious = int32(binary.NativeEndian.Uint32(me.data[24:28]))
+	result.score = int32(binary.NativeEndian.Uint32(me.data[16:20]))
+	result.scoreChanged = me.data[20] != 0
+	result.globalRankNew = int32(binary.NativeEndian.Uint32(me.data[24:28]))
+	result.globalRankPrevious = int32(binary.NativeEndian.Uint32(me.data[28:32]))
 	return result
 }
 
 type leaderboardEntry_t struct {
 	// m_steamIDUser: 8 bytes
 	// m_nGlobalRank: 4 bytes
-	// m_nScore:      4 bytes
+	// m_nScore:      4 bytes (8 bytes padded)
 	// m_cDetails:    4 bytes
 	// m_hUGC:        8 bytes
-	// total:         28 bytes
-	data [28]byte
+	// total:         40 bytes
+	data [40]byte
 }
 
 func (me leaderboardEntry_t) Read() leaderboardEntry {
@@ -75,7 +75,7 @@ func (me leaderboardEntry_t) Read() leaderboardEntry {
 	result.steamIDUser = CSteamID(binary.NativeEndian.Uint64(me.data[0:8]))
 	result.globalRank = int32(binary.NativeEndian.Uint32(me.data[8:12]))
 	result.score = int32(binary.NativeEndian.Uint32(me.data[12:16]))
-	result.details = int32(binary.NativeEndian.Uint32(me.data[16:20]))
-	result.UGC = UGCHandle_t(binary.NativeEndian.Uint64(me.data[20:28]))
+	result.details = int32(binary.NativeEndian.Uint32(me.data[20:24]))
+	result.UGC = UGCHandle_t(binary.NativeEndian.Uint64(me.data[24:32]))
 	return result
 }
