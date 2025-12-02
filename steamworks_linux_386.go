cac1225457c16de6e965c50fc58f1e6424cb743a
Michael Carlson <corfe83@gmail.com>
2025-12-01 16:08:38 -0800

    Re-add linux 32-bit support since purego supports that

---
+++ steamworks_linux_386.go
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2021 The go-steamworks Authors
+
+package steamworks
+
+import (
+	_ "embed"
+)
+
+//go:embed libsteam_api.so
+var libSteamAPI []byte
